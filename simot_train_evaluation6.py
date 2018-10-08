03d22d72070149e9a9a42cf44b4b7f40b7ed1a97
archord <xyag.902@163.com>
2018-10-08 20:13:36 +0800

    backup

--- simot_train_evaluation6.py
+++ simot_train_evaluation6.py
@@ -86,7 +86,8 @@ def test():
     tpath2 = "/home/xy/Downloads/myresource/deep_data2/gwac_ot2_apart"
     tpath3 = "/home/xy/Downloads/myresource/deep_data2/simot/rest_data_0929"
     
-    dateStr = datetime.strftime(datetime.now(), "%Y%m%d")
+    dateStr = '20181001'
+    #dateStr = datetime.strftime(datetime.now(), "%Y%m%d")
     workPath = "/home/xy/Downloads/myresource/deep_data2/simot/train_%s"%(dateStr)
     if not os.path.exists(workPath):
         os.system("mkdir %s"%(workPath))
@@ -254,6 +255,69 @@ def test():
             if tnum>showNum:
                 break
 
+def test2():
+
+    tpath2 = "/home/xy/Downloads/myresource/deep_data2/gwac_ot2_apart"
+    tpath3 = "/home/xy/Downloads/myresource/deep_data2/simot/rest_data_0929"
+    
+    dateStr = '20181001'
+    #dateStr = datetime.strftime(datetime.now(), "%Y%m%d")
+    workPath = "/home/xy/Downloads/myresource/deep_data2/simot/train_%s"%(dateStr)
+    if not os.path.exists(workPath):
+        os.system("mkdir %s"%(workPath))
+    print("work path is %s"%(workPath))
+    
+    X,Y,s2n = getData(tpath3, tpath2, workPath)
+    print(X.shape)
+    print(Y.shape)
+    print(s2n.shape)
+    
+    N_data = X.shape[0]
+    N_train = int(N_data * 0.9)
+    print("train: %d, test: %d"%(N_train, N_data-N_train))
+    X_train, Y_train, s2n_train = X[:N_train], Y[:N_train], s2n[:N_train]
+    X_test, Y_test, s2n_test = X[N_train:], Y[N_train:], s2n[N_train:]
+    
+    from keras.models import load_model
+    model = load_model("%s/model_128_5_RealFOT_8.h5"%(workPath))
+    Y_pred = model.predict(X_test)
+    pbb_threshold = 0.5
+    pred_labels = np.array((Y_pred[:, 1] > pbb_threshold), dtype = "int")
+    print("Correctly classified %d out of %d"%((pred_labels == Y_test[:, 1].astype(int)).sum(), Y_test.shape[0]))
+    print("accuracy = %f"%(1.*(pred_labels == Y_test[:, 1].astype(int)).sum() / Y_test.shape[0]))
+    
+    maxS2n = np.floor(np.max(s2n_test))
+    minS2n = np.floor(np.max(s2n_test))
+
+    TIdx = Y_test[:, 1]==1
+    FIdx = Y_test[:, 1]==0
+    T_pred_rst = pred_labels[TIdx]
+    F_pred_rst = pred_labels[FIdx]
+    print(T_pred_rst.shape)
+    print(F_pred_rst.shape)
+    
+    TP = ((T_pred_rst == 1).astype(int)).sum()
+    TN = ((F_pred_rst == 0).astype(int)).sum()
+    FP = ((F_pred_rst == 1).astype(int)).sum()
+    FN = ((T_pred_rst == 0).astype(int)).sum()
+    print("total=%d,TP=%d,TN=%d,FP=%d,FN=%d"%(Y_test.shape[0],TP, TN, FP, FN))
+    
+    accuracy = (TP+TN)*1.0/(TP+FN+TN+FP)
+    precision = (TP)*1.0/(TP+FP)
+    recall = (TP)*1.0/(TP+FN)
+    f1_score = 2.0*(precision*recall)/(precision+recall)
+    print("accuracy=%f%%"%(accuracy*100))
+    print("precision=%f%%"%(precision*100))
+    print("recall=%f%%"%(recall*100))
+    print("f1_score=%f%%"%(f1_score*100))
+    
+    falseImg = X_test[pred_labels != Y_test[:, 1]]
+    falseLabel = Y_test[pred_labels != Y_test[:, 1]]
+    falsePred = Y_pred[pred_labels != Y_test[:, 1]]
+    falseS2n= s2n_test[pred_labels != Y_test[:, 1]]
+    
+   
+            
 def realDataTest():
         
     #dateStr = datetime.strftime(datetime.now(), "%Y%m%d")
