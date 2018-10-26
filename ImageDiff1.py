62e09557cc7436f8a07d98688aaa8db431c37e1b
fireball <273809188@qq.com>
2018-10-26 22:20:16 +0800

    backup

---
+++ ImageDiff1.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+import scipy as S
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from random import randint, random
+import pandas as pd
+import sys
+import math
+import os
+import time
+import logging
+import shutil
+import subprocess
+from gwac_util import zscale_image, selectTempOTs, filtOTs, filtByEllipticity, genFinalOTDs9Reg, getThumbnail_,getThumbnail
+import scipy.ndimage
+
+
+class ImageDiff(object):
+    def __init__(self):
+        
+        self.verbose = True
+        
+        self.varDir = "/home/xy/Downloads/myresource/deep_data2/simulate_tools"
+        self.srcDir = "/home/xy/Downloads/myresource/deep_data2/G180216/17320495.0" # ls CombZ_*fit
+        self.srcDirBad = "/home/xy/Downloads/myresource/deep_data2/G180216/17320495.0_bad"
+        self.tmpDir="/run/shm/gwacsim"
+        self.destDir="/home/xy/Downloads/myresource/deep_data2/simot/rest_data_0929"
+        self.matchProgram="/home/xy/program/netbeans/C/CrossMatchLibrary/dist/Debug/GNU-Linux/crossmatchlibrary"
+        self.imgDiffProgram="/home/xy/program/C/hotpants/hotpants"
+                
+        if not os.path.exists(self.tmpDir):
+            os.system("mkdir %s"%(self.tmpDir))
+        if not os.path.exists(self.destDir):
+            os.system("mkdir %s"%(self.destDir))
+        if not os.path.exists(self.srcDirBad):
+            os.system("mkdir %s"%(self.srcDirBad))
+            
+        self.objectImg = 'oi.fit'
+        self.templateImg = 'ti.fit'
+        self.objectImgSim = 'oi_simaddstar1.fit'
+        self.objTmpResi = 'otr.fit'
+        self.simTmpResi = 'str.fit'
+        self.objectImgSubBkg = 'oi_subkg.fit'
+        self.templateImgSubBkg = 'ti_subkg.fit'
+        self.objectImgSimSubBkg = 'oi_simaddstar1_subkg.fit'
+        
+        self.objectImgSimAdd = 'oisa.cat'
+        self.objectImgCat = 'oi.cat'
+        self.templateImgCat = 'ti.cat'
+        self.objectImgSimCat = 'ois.cat'
+        self.objTmpResiCat = 'otr.cat'
+        self.simTmpResiCat = 'str.cat'
+        
+        self.subImgSize = 21
+        self.r1 = 1
+        self.r5 = 5
+        self.r10 = 10
+        self.r16 = 16
+        self.r24 = 24
+        self.r32 = 32
+        self.r46 = 46
+        
+        self.log = logging.getLogger() #create logger
+        self.log.setLevel(logging.DEBUG) #set level of logger
+        formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s") #set format of logger
+        logging.Formatter.converter = time.gmtime #convert time in logger to UCT
+        #filehandler = logging.FileHandler("%s/otSim.log"%(self.destDir), 'w+')
+        filehandler = logging.FileHandler("%s/otSim.log"%(self.tmpDir), 'w+')
+        filehandler.setFormatter(formatter) #add format to log file
+        self.log.addHandler(filehandler) #link log file to logger
+        if self.verbose:
+            streamhandler = logging.StreamHandler() #create print to screen logging
+            streamhandler.setFormatter(formatter) #add format to screen logging
+            self.log.addHandler(streamhandler) #link logger to screen logging
+    
+    #catalog self match
+    def runSelfMatch(self, fname, mchRadius):
+        
+        outpre= fname.split(".")[0]
+        fullPath = "%s/%s"%(self.tmpDir, fname)
+        
+        # run sextractor from the unix command line
+        cmd = [self.matchProgram, fullPath, str(mchRadius), '4', '5', '39']
+        self.log.debug(cmd)
+           
+        # run command
+        process=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        (stdoutstr,stderrstr) = process.communicate()
+        status = process.returncode
+        self.log.info(stdoutstr)
+        self.log.info(stderrstr)
+        
+        mchFile = "%s_sm%d.cat"%(outpre,mchRadius)
+        nmhFile = "%s_sn%d.cat"%(outpre,mchRadius)
+        mchFilePath = "%s/%s"%(self.tmpDir,mchFile)
+        nmhFilePath = "%s/%s"%(self.tmpDir,nmhFile)
+        if os.path.exists(mchFilePath) and os.path.exists(nmhFilePath) and status==0:
+            self.log.debug("run self match success.")
+            self.log.debug("generate matched file %s"%(mchFile))
+            self.log.debug("generate not matched file %s"%(nmhFile))
+        else:
+            self.log.error("self match failed.")
+        
+        return mchFile, nmhFile
+    
+    #crossmatch 
+    def runCrossMatch(self, objCat, tmpCat, mchRadius):
+        
+        objpre= objCat.split(".")[0]
+        tmppre= tmpCat.split(".")[0]
+        objFPath = "%s/%s"%(self.tmpDir, objCat)
+        tmpFPath = "%s/%s"%(self.tmpDir, tmpCat)
+        outFPath = "%s/%s_%s.out"%(self.tmpDir, objpre,tmppre)
+        
+        # run sextractor from the unix command line
+        cmd = [self.matchProgram, tmpFPath, objFPath, outFPath, str(mchRadius), '4', '5', '39']
+        self.log.debug(cmd)
+           
+        # run command
+        process=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        (stdoutstr,stderrstr) = process.communicate()
+        status = process.returncode
+        self.log.info(stdoutstr)
+        self.log.info(stderrstr)
+        
+        mchPair = "%s_%s_cm%d.pair"%(objpre,tmppre,mchRadius)
+        mchFile = "%s_%s_cm%d.cat"%(objpre,tmppre,mchRadius)
+        nmhFile = "%s_%s_cn%d.cat"%(objpre,tmppre,mchRadius)
+        mchPairPath = "%s/%s"%(self.tmpDir,mchPair)
+        mchFilePath = "%s/%s"%(self.tmpDir,mchFile)
+        nmhFilePath = "%s/%s"%(self.tmpDir,nmhFile)
+        if os.path.exists(mchPairPath) and os.path.exists(mchFilePath) and os.path.exists(nmhFilePath) and status==0:
+            self.log.debug("run catalog match success.")
+            self.log.debug("generate pair file %s"%(mchPair))
+            self.log.debug("generate matched file %s"%(mchFile))
+            self.log.debug("generate not matched file %s"%(nmhFile))
+        else:
+            self.log.error("cross match failed.")
+        
+        return mchFile, nmhFile, mchPair
+    
+    #source extract
+    def runSextractor(self, fname, sexConf=['-DETECT_MINAREA','5','-DETECT_THRESH','3','-ANALYSIS_THRESH','3']):
+        
+        outpre= fname.split(".")[0]
+        fullPath = "%s/%s"%(self.tmpDir, fname)
+        outFile = "%s.cat"%(outpre)
+        outFPath = "%s/%s"%(self.tmpDir, outFile)
+        outCheckPath = "%s/%s_subkg.fit"%(self.tmpDir, outpre)
+        cnfPath = "%s/config/OTsearch.sex"%(self.varDir)
+        
+        #DETECT_MINAREA   5              # minimum number of pixels above threshold
+        #DETECT_THRESH    3.0             #  <sigmas>  or  <threshold>,<ZP>  in  mag.arcsec-2  
+        #ANALYSIS_THRESH  3.0
+        # run sextractor from the unix command line
+        cmd = ['sex', fullPath, '-c', cnfPath, '-CATALOG_NAME', outFPath, '-CHECKIMAGE_TYPE', '-BACKGROUND', '-CHECKIMAGE_NAME', outCheckPath]
+        cmd = cmd + sexConf
+        self.log.debug(cmd)
+           
+        # run command
+        process=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        (stdoutstr,stderrstr) = process.communicate()
+        status = process.returncode
+        #self.log.info(stdoutstr)
+        #self.log.info(stderrstr)
+        
+        if os.path.exists(outFPath) and status==0:
+            self.log.debug("run sextractor success.")
+            self.log.debug("generate catalog %s"%(outFPath))
+        else:
+            self.log.error("sextractor failed.")
+            
+        return outFile
+        
+    #hotpants
+    def runHotpants(self, objImg, tmpImg):
+        
+        objpre= objImg.split(".")[0]
+        tmppre= tmpImg.split(".")[0]
+        objFPath = "%s/%s"%(self.srcDir, objImg)
+        tmpFPath = "%s/%s"%(self.srcDir, tmpImg)
+        outFile = "%s_%s_resi.fit"%(objpre,tmppre)
+        outFPath = "%s/%s"%(self.tmpDir, outFile)
+        
+        # run sextractor from the unix command line
+        #/home/xy/program/C/hotpants/hotpants -inim oi.fit -tmplim ti.fit -outim oi_ti_resi.fit -v 0 -nrx 4 -nry 4
+        cmd = [self.imgDiffProgram, '-inim', objFPath, '-tmplim', tmpFPath, '-outim', 
+                 outFPath, '-v', '0', '-nrx', '4', '-nry', '4']
+        self.log.debug(cmd)
+           
+        # run command
+        process=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        (stdoutstr,stderrstr) = process.communicate()
+        status = process.returncode
+        #self.log.info(stdoutstr)
+        #self.log.info(stderrstr)
+        
+        if os.path.exists(outFPath) and status==0:
+            self.log.debug("run hotpants success.")
+            self.log.debug("generate diff residual image %s"%(outFPath))
+        else:
+            self.log.error("hotpants failed.")
+            
+        return outFile
+        
+    def removeHeader(self, fname):
+        
+        fullPath = "%s/%s"%(self.tmpDir, fname)
+        
+        fname='G022_mon_objt_180226T17492514_2.fits'
+        keyword=['WCSASTRM','WCSDIM','CTYPE1','CTYPE2',
+                 'CRVAL1','CRVAL2','CRPIX1','CRPIX2',
+                 'CD1_1','CD1_2','CD2_1','CD2_2','WAT0_001',
+                 'WAT1_001','WAT1_002','WAT1_003','WAT1_004','WAT1_005','WAT1_006','WAT1_007','WAT1_008',
+                 'WAT2_001','WAT2_002','WAT2_003','WAT2_004','WAT2_005','WAT2_006','WAT2_007','WAT2_008']
+    
+        with fits.open(fullPath, mode='update') as hdul:
+            hdr = hdul[0].header
+            for kw in keyword:
+                hdr.remove(kw,ignore_missing=True)
+            hdul.flush()
+            hdul.close()     
+        
+    def diff(self, objImg, tempImg):
+        
+        objPath = "%s/%s"%(self.srcDir, objImg)
+        tmpPath = "%s/%s"%(self.srcDir, tempImg)
+        objData = fits.getdata(objPath)
+        #print("**min=%f,max=%f"%(np.min(objData), np.max(objData)))
+        tempData = fits.getdata(tmpPath)
+        #print("**min=%f,max=%f"%(np.min(tempData), np.max(tempData)))
+        diffData = objData.astype(np.float) - tempData.astype(np.float) 
+        #print("**min=%f,max=%f"%(np.min(diffData), np.max(diffData)))
+        #diffData = tempData - objData
+        diffData[diffData<0] = 0
+        
+        timg = getThumbnail_(diffData, stampSize=(100,100), grid=(5, 5), innerSpace = 1)
+        return timg
+    
+    def test(self):
+                    
+        oImg = 'CombZ_0.fit'
+        tImg = 'CombZ_temp.fit'
+        
+        os.system("rm -rf %s/*"%(self.tmpDir))
+                
+        os.system("cp %s/%s %s/%s"%(self.srcDir, oImg, self.tmpDir, self.objectImg))
+        #os.system("cp %s/%s %s/%s"%(self.srcDir, tImg, self.tmpDir, self.templateImg))
+        
+        self.removeHeader(self.objectImg)
+        self.removeHeader(self.templateImg)
+        
+        self.objectImgCat = self.runSextractor(self.objectImg)
+        #self.templateImgCat = self.runSextractor(self.templateImg)
+
+    def batchDiff(self):
+        
+        flist = os.listdir(self.srcDir)
+        flist.sort()
+        
+        imgs = []
+        for tfilename in flist:
+            if tfilename.find("fit")>-1 and tfilename.find("temp")==-1:
+                imgs.append(tfilename)
+        
+        templateImg = imgs[0]
+        print("template is %s"%(templateImg))
+        
+        timg = getThumbnail(self.srcDir, templateImg, stampSize=(100,100), grid=(5, 5), innerSpace = 1)
+        timg = scipy.ndimage.zoom(timg, 4, order=0)
+        plt.figure(figsize = (12, 12))
+        plt.imshow(timg, cmap='gray')
+        plt.show()
+        
+        for i in range(1):
+            os.system("rm -rf %s/*"%(self.tmpDir))
+            #tIdx = 5 + i*10
+            tIdx = 10
+            objImg = imgs[tIdx]
+            print("process %d: %s"%(tIdx, objImg))
+        
+            timg = self.diff(objImg, templateImg)
+            #timgPath = self.runHotpants(objImg, templateImg)
+            #timg = getThumbnail(self.tmpDir, timgPath, stampSize=(100,100), grid=(5, 5), innerSpace = 1)
+            timg = scipy.ndimage.zoom(timg, 4, order=0)
+
+            plt.figure(figsize = (12, 12))
+            plt.imshow(timg, cmap='gray')
+            plt.show()
+    
+            
+if __name__ == "__main__":
+    
+    imgDiff = ImageDiff()
+    imgDiff.batchDiff()
+    
