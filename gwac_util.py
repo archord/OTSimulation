3b82d6d589510805019f0788a11ce0d65406cbf2
fireball <273809188@qq.com>
2018-10-26 23:43:07 +0800

    backup

--- gwac_util.py
+++ gwac_util.py
@@ -70,7 +70,8 @@ def genPSFView(psfImgs, innerSpace = 1):
 
 '''
 对图像均匀接取grid=width_num*height_num个子窗口图像，每个窗口图像的大小为stampSize=(stampW, stapmH)
-没有考虑grid=(1,1)的情形
+grid可以等于(1,1)
+innerSpace可以等于0
 '''
 def getThumbnail(imgPath, imgName, stampSize=(500,500), grid=(3, 3), innerSpace = 1, contrast=0.25):
     
@@ -82,7 +83,8 @@ def getThumbnail(imgPath, imgName, stampSize=(500,500), grid=(3, 3), innerSpace 
                 
 '''
 对图像均匀接取grid=width_num*height_num个子窗口图像，每个窗口图像的大小为stampSize=(stampW, stapmH)
-没有考虑grid=(1,1)的情形
+grid可以等于(1,1)
+innerSpace可以等于0
 '''
 def getThumbnail_(tdata, stampSize=(500,500), grid=(3, 3), innerSpace = 1, contrast=0.25):
     
@@ -91,32 +93,116 @@ def getThumbnail_(tdata, stampSize=(500,500), grid=(3, 3), innerSpace = 1, contr
     imgH = imgSize[0]
     halfStampW = int(stampSize[0]/2)
     halfStampH = int(stampSize[1]/2)
+
+    if grid[0]>1 and grid[1]>1:
+        startX = halfStampW
+        endX = imgW - halfStampW
+        startY = halfStampH
+        endY = imgH - halfStampH
+        XInterval = int((endX-startX)/(grid[0]-1))
+        YInterval = int((endY-startY)/(grid[1]-1))
+        
+        subRegions = []
+        for y in range(grid[1]):
+            centerY = startY + y*YInterval
+            minY = centerY - halfStampH
+            maxY = centerY + halfStampH
+            if minY<0:
+                minY=0
+            if maxY>imgH:
+                maxY = imgH
+            for x in range(grid[0]):
+                centerX = startX + x*XInterval
+                minX = centerX - halfStampW
+                maxX = centerX + halfStampW
+                if minX<0:
+                    minX=0
+                if maxX>imgW:
+                    maxX = imgW
+                subRegions.append((minY, maxY, minX, maxX))
+        
+        #print(subRegions)
+        stampImgs = []
+        for treg in subRegions:
+            timg = tdata[treg[0]:treg[1], treg[2]:treg[3]]
+            timgz = zscale_image(timg, contrast=0.25)
+            if timgz.shape[0] == 0:
+                timgz = timg
+                tmin = np.min(timgz)
+                tmax = np.max(timgz)
+                timgz=(((timgz-tmin)/(tmax-tmin))*255).astype(np.uint8)
+            stampImgs.append(timgz)
+        
+        for y in range(grid[1]):
+            for x in range(grid[0]):
+                tidx = y*grid[0] + x
+                timg = stampImgs[tidx]
+                if x ==0:
+                    rowImg = timg
+                else:
+                    xspace = np.ones((timg.shape[0],innerSpace), np.uint8)*255
+                    rowImg = np.concatenate((rowImg, xspace, timg), axis=1)
+            if y ==0:
+                conImg = rowImg
+            else:
+                yspace = np.ones((innerSpace,rowImg.shape[1]), np.uint8)*255
+                conImg = np.concatenate((conImg, yspace, rowImg), axis=0)
+    else:
+        ctrX = math.ceil(imgW/2)-1
+        ctrY = math.ceil(imgH/2)-1
+        startX = ctrX - halfStampW
+        endX = ctrX + halfStampW
+        startY = ctrY - halfStampH
+        endY = ctrY + halfStampH
+        if startX<0:
+            startX=0
+        if endX>=imgH:
+            endX = imgH-1
+        if startY<0:
+            startY=0
+        if endY>=imgW:
+            endY = imgW-1
+        timg = tdata[startY:endY, startX:endX]
+        timgz = zscale_image(timg, contrast=0.25)
+        if timgz.shape[0] == 0:
+            timgz = timg
+            tmin = np.min(timgz)
+            tmax = np.max(timgz)
+            timgz=(((timgz-tmin)/(tmax-tmin))*255).astype(np.uint8)
+        conImg = timgz
+        
+    return conImg
+       
     
-    startX = halfStampW
-    endX = imgW - halfStampW
-    startY = halfStampH
-    endY = imgH - halfStampH
-    
-    XInterval = int((endX-startX)/(grid[0]-1))
-    YInterval = int((endY-startY)/(grid[1]-1))
+def getFullThumbnail(imgPath, imgName, grid=(8, 8), contrast=0.25):
+        
+    fpath = "%s/%s"%(imgPath, imgName)
+    tdata = fits.getdata(fpath)
+    imgSize = tdata.shape
+    imgW = imgSize[1]
+    imgH = imgSize[0]
+    XInterval = math.floor(imgW/grid[0])
+    YInterval = math.floor(imgH/grid[1])
     
     subRegions = []
     for y in range(grid[1]):
-        centerY = startY + y*YInterval
-        minY = centerY - halfStampH
-        maxY = centerY + halfStampH
+        minY = y*YInterval
+        maxY = (y+1)*YInterval
+        if y==grid[1]-1:
+            maxY = imgH
         if minY<0:
             minY=0
-        if maxY>=imgH:
-            maxY = imgH-1
+        if maxY>imgH:
+            maxY = imgH
         for x in range(grid[0]):
-            centerX = startX + x*XInterval
-            minX = centerX - halfStampW
-            maxX = centerX + halfStampW
+            minX = x*XInterval
+            maxX = (x+1)*XInterval
+            if x==grid[0]-1:
+                maxX = imgW
             if minX<0:
                 minX=0
-            if maxX>=imgW:
-                maxX = imgW-1
+            if maxX>imgW:
+                maxX = imgW
             subRegions.append((minY, maxY, minX, maxX))
     
     #print(subRegions)
@@ -138,15 +224,13 @@ def getThumbnail_(tdata, stampSize=(500,500), grid=(3, 3), innerSpace = 1, contr
             if x ==0:
                 rowImg = timg
             else:
-                xspace = np.ones((timg.shape[0],innerSpace), np.uint8)*255
-                rowImg = np.concatenate((rowImg, xspace, timg), axis=1)
+                rowImg = np.concatenate((rowImg, timg), axis=1)
         if y ==0:
             conImg = rowImg
         else:
-            yspace = np.ones((innerSpace,rowImg.shape[1]), np.uint8)*255
-            conImg = np.concatenate((conImg, yspace, rowImg), axis=0)
-    return conImg
-            
+            conImg = np.concatenate((conImg, rowImg), axis=0)
+
+    return conImg     
 
 def zscale_image(input_img, contrast=0.25):
 
