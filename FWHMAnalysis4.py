973a4a77dcfcce6c8baaadf4a04b0158604b95d0
fireball <273809188@qq.com>
2018-11-20 23:23:05 +0800

    Update FWHMAnalysis4.py

--- FWHMAnalysis4.py
+++ FWHMAnalysis4.py
@@ -12,7 +12,11 @@ from PIL import Image
 from astropy.stats import sigma_clip
 from mpl_toolkits.mplot3d import Axes3D
 
-
+def FWHM_moffat(gamma, alpha):
+    return 2*gamma * np.sqrt( 2**(1/alpha) - 1)
+    
+def gamma_moffat(fwhm, alpha):
+    return fwhm / (2*np.sqrt( 2**(1/alpha) - 1))
 
 def fwhmrGridStatistic(catfile, size, gridNum=40):
     
@@ -72,16 +76,16 @@ def fwhmrGridStatistic(catfile, size, gridNum=40):
             p_init = models.Moffat2D(amplitude=Z[5,5],
                            x_0=5, y_0=5,
                            gamma=fwhm,
-                           alpha=1.0)
+                           alpha=3.0)
             
-            fit_p = fitting.LevMarLSQFitter()
-            pZ = fit_p(p_init, X, Y, Z)
-            Z1 = pZ(X,Y)
+            fitter = fitting.LevMarLSQFitter()
+            p_fit = fitter(p_init, X, Y, Z)
+            Z1 = p_fit(X,Y)
+            print(p_fit)
             
-            print(p_init.alpha)
-            print(p_init.amplitude)
-            print(p_init.fwhm)
-            print(p_init.gamma)
+            print(p_fit.fwhm)
+            tfwhm = FWHM_moffat(p_fit.gamma, p_fit.alpha)
+            print(tfwhm)
             
             fig = plt.figure()
             fig = plt.figure()
@@ -99,10 +103,70 @@ def test2():
     #size=(3056,3056)
     osn16 = r"E:\fwhm\data1\oi.cat" # jfov 
     fwhmrGridStatistic(osn16, size, gridNum=40)
+
+def norm_moffat(width, power):
+    from scipy.special import gamma as G
+    return G(power) / (width * np.sqrt(np.pi) * G(power - 1/2))
+    
+def test1():
+    
+    from astropy.modeling.models import Moffat1D
+
+    plt.figure()
+    s1 = Moffat1D()
+    r = np.arange(-5, 5, .01)
+    
+    for factor in range(1, 5):
+        s1.gamma=4.18/factor
+        s1.alpha=6.754
+        print(s1.fwhm)
+        #plt.plot(r, norm_moffat(s1.gamma, s1.alpha)*s1(r), color=str(0.1 * factor), lw=2)
+        plt.plot(r, norm_moffat(s1.gamma, s1.alpha)*s1(r), lw=2)
+    
+    plt.grid()
+    plt.axis([-1, 1, -0.5, 2])
+    plt.show()
+    
+def test3():
+    from astropy.modeling.functional_models import Gaussian1D
+    
+    def norm_gauss(sigma):
+        return 1/np.sqrt(2 * np.pi * sigma**2)
+    
+    
+    x = np.arange(-8,8,0.1)
+    
+    ax1 = plt.subplot(1,1,1)
+    for s in range(1, 5):
+        gauss  = Gaussian1D(amplitude=1, mean=0, stddev=s)    
+        ax1.plot(x, norm_gauss(s)*gauss(x), ls=":", 
+                 label="sigma={0:.0f}, HWHM={1:.1f}".format(s, 2.355*s/2))
+        ax1.plot(x, gauss(x),label="sigma={0:.0f}, HWHM={1:.1f}".format(s, 2.355*s/2))
         
-        
-            
+       
+def test4():
+    
+    from astropy.modeling.models import Moffat1D
+
+    plt.figure()
+    s1 = Moffat1D()
+    r = np.arange(-5, 5, .01)
+    
+    for fwhm in np.arange(1.8, 2.1, 0.2):
+        s1.alpha=6.754
+        s1.gamma=gamma_moffat(fwhm, s1.alpha)
+        #print("%.3f, %.3f"%(s1.fwhm,fwhm))
+        #plt.plot(r, norm_moffat(s1.gamma, s1.alpha)*s1(r), color=str(0.1 * factor), lw=2)
+        plt.plot(r, norm_moffat(s1.gamma, s1.alpha)*s1(r), lw=2,
+                 label='HWHM=%.3f'%(s1.fwhm))
+    
+    plt.grid()
+    #plt.axis([-1, 1, -0.5, 2])
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+     
 if __name__ == "__main__":
     
-    test2()
+    test4()
     
