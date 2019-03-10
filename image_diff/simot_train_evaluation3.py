a1edcfe6284847e62d1b58fd94d49fb1d7c1081a
fireball <273809188@qq.com>
2019-03-10 22:26:22 +0800

    Update simot_train_evaluation3.py

--- image_diff/simot_train_evaluation3.py
+++ image_diff/simot_train_evaluation3.py
@@ -10,7 +10,7 @@ import keras
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Activation, Flatten, Input
 from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, Concatenate, Cropping2D
-from DataPreprocess import saveImgs, getFinalTestData, getRealData
+from DataPreprocess import saveImgs, getFinalTestData, getRealData, getData2
 from gwac_util import zscale_image
 import traceback
     
@@ -30,9 +30,89 @@ def modelCompare():
     
     tModelNamePart1 = "64_100_fot10w_%s_dropout"%(dateStr)
     tModelNamePart2 = "8_100_fot10w_%s_dropout"%(dateStr)
-    test(totpath, fotpath1, fotpath2, realDataPath, workPath, tModelNamePart1)
-    test(totpath, fotpath1, fotpath2, realDataPath, workPath, tModelNamePart2)
+    #test(totpath, fotpath1, fotpath2, realDataPath, workPath, tModelNamePart1)
+    #test(totpath, fotpath1, fotpath2, realDataPath, workPath, tModelNamePart2)
     #test2(totpath, fotpath1, fotpath2, realDataPath, workPath, tModelNamePart1, tModelNamePart2)
+    
+    tSampleNamePart = "64_fot10w_%s"%(dateStr)
+    #test3(totpath, fotpath2, workPath, tSampleNamePart, tModelNamePart1, tModelNamePart2)
+    test4(totpath, fotpath2, workPath, tSampleNamePart, tModelNamePart1)
+    test4(totpath, fotpath2, workPath, tSampleNamePart, tModelNamePart2)
+    
+def test3(totpath, fotpath, workPath, tSampleNamePart, tModelNamePart1, tModelNamePart2):
+        
+    X,Y,s2n = getData2(totpath, fotpath, workPath, tSampleNamePart)
+    print(X.shape)
+    print(Y.shape)
+    print(s2n.shape)
+
+    N_data = X.shape[0]
+    N_train = int(N_data * 0.9)
+    print("train: %d, test: %d"%(N_train, N_data-N_train))
+    X_test, Y_test, s2n_test = X[N_train:], Y[N_train:], s2n[N_train:]
+    
+    from keras.models import load_model
+    modelName1 = "model_RealFOT_%s.h5"%(tModelNamePart1)
+    model = load_model("%s/%s"%(workPath, modelName1))
+    Y_pred = model.predict(X_test)
+    
+    pbb_threshold = 0.5
+    pred_labels = np.array((Y_pred[:, 1] > pbb_threshold), dtype = "int")
+    print("Correctly classified %d out of %d"%((pred_labels == Y_test[:, 1].astype(int)).sum(), Y_test.shape[0]))
+    print("accuracy = %f"%(1.*(pred_labels == Y_test[:, 1].astype(int)).sum() / Y_test.shape[0]))
+
+    TIdx = Y_test[:, 1]==1
+    FIdx = Y_test[:, 1]==0
+    T_pred_rst = pred_labels[TIdx]
+    F_pred_rst = pred_labels[FIdx]
+    print(T_pred_rst.shape)
+    print(F_pred_rst.shape)
+    
+    TP = ((T_pred_rst == 1).astype(int)).sum()
+    TN = ((F_pred_rst == 0).astype(int)).sum()
+    FP = ((F_pred_rst == 1).astype(int)).sum()
+    FN = ((T_pred_rst == 0).astype(int)).sum()
+    print("total=%d,TP=%d,TN=%d,FP=%d,FN=%d"%(Y_test.shape[0],TP, TN, FP, FN))
+    
+    accuracy = (TP+TN)*1.0/(TP+FN+TN+FP)
+    precision = (TP)*1.0/(TP+FP)
+    recall = (TP)*1.0/(TP+FN)
+    f1_score = 2.0*(precision*recall)/(precision+recall)
+    print("accuracy=%f%%"%(accuracy*100))
+    print("precision=%f%%"%(precision*100))
+    print("recall=%f%%"%(recall*100))
+    print("f1_score=%f%%"%(f1_score*100))
+    
+    modelName1 = "model_RealFOT_%s.h5"%(tModelNamePart2)
+    model = load_model("%s/%s"%(workPath, modelName1))
+    Y_pred = model.predict(X_test)
+    
+    pbb_threshold = 0.5
+    pred_labels = np.array((Y_pred[:, 1] > pbb_threshold), dtype = "int")
+    print("Correctly classified %d out of %d"%((pred_labels == Y_test[:, 1].astype(int)).sum(), Y_test.shape[0]))
+    print("accuracy = %f"%(1.*(pred_labels == Y_test[:, 1].astype(int)).sum() / Y_test.shape[0]))
+
+    TIdx = Y_test[:, 1]==1
+    FIdx = Y_test[:, 1]==0
+    T_pred_rst = pred_labels[TIdx]
+    F_pred_rst = pred_labels[FIdx]
+    print(T_pred_rst.shape)
+    print(F_pred_rst.shape)
+    
+    TP = ((T_pred_rst == 1).astype(int)).sum()
+    TN = ((F_pred_rst == 0).astype(int)).sum()
+    FP = ((F_pred_rst == 1).astype(int)).sum()
+    FN = ((T_pred_rst == 0).astype(int)).sum()
+    print("total=%d,TP=%d,TN=%d,FP=%d,FN=%d"%(Y_test.shape[0],TP, TN, FP, FN))
+    
+    accuracy = (TP+TN)*1.0/(TP+FN+TN+FP)
+    precision = (TP)*1.0/(TP+FP)
+    recall = (TP)*1.0/(TP+FN)
+    f1_score = 2.0*(precision*recall)/(precision+recall)
+    print("accuracy=%f%%"%(accuracy*100))
+    print("precision=%f%%"%(precision*100))
+    print("recall=%f%%"%(recall*100))
+    print("f1_score=%f%%"%(f1_score*100))
     
 def test2(totpath, fotpath1, fotpath2, realDataPath, workPath, tModelNamePart1, tModelNamePart2):
         
@@ -108,7 +188,7 @@ def test2(totpath, fotpath1, fotpath2, realDataPath, workPath, tModelNamePart1, 
     
     
 def test(totpath, fotpath1, fotpath2, realDataPath, workPath, tModelNamePart):
-        
+    
     X,Y,s2n = getFinalTestData(totpath, fotpath1, fotpath2, realDataPath, workPath)
     print(X.shape)
     print(Y.shape)
@@ -269,6 +349,170 @@ def test(totpath, fotpath1, fotpath2, realDataPath, workPath, tModelNamePart):
             if tnum>showNum:
                 break
 
+def test4(totpath, fotpath, workPath, tSampleNamePart, tModelNamePart):
+        
+    X,Y,s2n = getData2(totpath, fotpath, workPath, tSampleNamePart)
+    print(X.shape)
+    print(Y.shape)
+    print(s2n.shape)
+
+    N_data = X.shape[0]
+    N_train = int(N_data * 0.9)
+    print("train: %d, test: %d"%(N_train, N_data-N_train))
+    X_test, Y_test, s2n_test = X[N_train:], Y[N_train:], s2n[N_train:]
+    
+    from keras.models import load_model
+    modelName1 = "model_RealFOT_%s.h5"%(tModelNamePart)
+    model = load_model("%s/%s"%(workPath, modelName1))
+    Y_pred = model.predict(X_test)
+    
+    pbb_threshold = 0.5
+    pred_labels = np.array((Y_pred[:, 1] > pbb_threshold), dtype = "int")
+    print("Correctly classified %d out of %d"%((pred_labels == Y_test[:, 1].astype(int)).sum(), Y_test.shape[0]))
+    print("accuracy = %f"%(1.*(pred_labels == Y_test[:, 1].astype(int)).sum() / Y_test.shape[0]))
+
+    TIdx = Y_test[:, 1]==1
+    FIdx = Y_test[:, 1]==0
+    T_pred_rst = pred_labels[TIdx]
+    F_pred_rst = pred_labels[FIdx]
+    print(T_pred_rst.shape)
+    print(F_pred_rst.shape)
+    
+    TP = ((T_pred_rst == 1).astype(int)).sum()
+    TN = ((F_pred_rst == 0).astype(int)).sum()
+    FP = ((F_pred_rst == 1).astype(int)).sum()
+    FN = ((T_pred_rst == 0).astype(int)).sum()
+    print("total=%d,TP=%d,TN=%d,FP=%d,FN=%d"%(Y_test.shape[0],TP, TN, FP, FN))
+    
+    accuracy = (TP+TN)*1.0/(TP+FN+TN+FP)
+    precision = (TP)*1.0/(TP+FP)
+    recall = (TP)*1.0/(TP+FN)
+    f1_score = 2.0*(precision*recall)/(precision+recall)
+    print("accuracy=%f%%"%(accuracy*100))
+    print("precision=%f%%"%(precision*100))
+    print("recall=%f%%"%(recall*100))
+    print("f1_score=%f%%"%(f1_score*100))
+    
+    falseImg = X_test[pred_labels != Y_test[:, 1]]
+    falseLabel = Y_test[pred_labels != Y_test[:, 1]]
+    falsePred = Y_pred[pred_labels != Y_test[:, 1]]
+    falseS2n= s2n_test[pred_labels != Y_test[:, 1]]
+    
+    tnum1 = 0
+    tnum2 = 0
+    for i in range(falseImg.shape[0]):
+        if falseLabel[i, 1]==1:
+            tnum1 = tnum1 + 1
+        else:
+            tnum2 = tnum2 + 1
+    print("total %d,  miss classified %d"%(Y_test.shape[0], falseImg.shape[0]))
+    print("True classified as False %d"%(tnum1))
+    print("False classified as True %d"%(tnum2))
+    print("\n\n***********************")
+    print("image of True classified as False")
+    for i in range(falseImg.shape[0]):
+        if falseLabel[i, 1]==1:
+            objWidz = zscale_image(falseImg[i][0])
+            tmpWidz = zscale_image(falseImg[i][1])
+            resiWidz = zscale_image(falseImg[i][2])
+            if objWidz.shape[0] == 0:
+                objWidz = falseImg[i][0]
+            if tmpWidz.shape[0] == 0:
+                tmpWidz = falseImg[i][1]
+            if resiWidz.shape[0] == 0:
+                resiWidz = falseImg[i][2]
+            plt.clf()
+            fig, axes = plt.subplots(1, 3, figsize=(6, 2))
+            axes.flat[0].imshow(objWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[1].imshow(tmpWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[2].imshow(resiWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[2].set_title("predicted pbb = " + str(np.round(falsePred[i][1], 2)) + 
+                                          ", label = " + str(falseLabel[i, 1]) + 
+                                          ", s2n = " + str(falseS2n[i]))
+            plt.show()
+    
+    print("\n\n***********************")
+    print("image of False classified as True")
+    for i in range(falseImg.shape[0]):
+        if falseLabel[i, 1]==0:
+            objWidz = zscale_image(falseImg[i][0])
+            tmpWidz = zscale_image(falseImg[i][1])
+            resiWidz = zscale_image(falseImg[i][2])
+            if objWidz.shape[0] == 0:
+                objWidz = falseImg[i][0]
+            if tmpWidz.shape[0] == 0:
+                tmpWidz = falseImg[i][1]
+            if resiWidz.shape[0] == 0:
+                resiWidz = falseImg[i][2]
+            plt.clf()
+            fig, axes = plt.subplots(1, 3, figsize=(6, 2))
+            axes.flat[0].imshow(objWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[1].imshow(tmpWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[2].imshow(resiWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[2].set_title("predicted pbb = " + str(np.round(falsePred[i][1], 2)) + 
+                                          ", label = " + str(falseLabel[i, 1]) + 
+                                          ", s2n = " + str(falseS2n[i]))
+            plt.show()
+     
+    trueImg = X_test[pred_labels == Y_test[:, 1]]
+    trueLabel = Y_test[pred_labels == Y_test[:, 1]]
+    truePred = Y_pred[pred_labels == Y_test[:, 1]]
+    trueS2n= s2n_test[pred_labels == Y_test[:, 1]]
+    
+    showNum=10
+    tnum = 0
+    print("\n\n***********************")
+    print("image of True classified as True")
+    for i in range(trueImg.shape[0]):
+        if trueLabel[i, 1]==1:
+            objWidz = zscale_image(trueImg[i][0])
+            tmpWidz = zscale_image(trueImg[i][1])
+            resiWidz = zscale_image(trueImg[i][2])
+            if objWidz.shape[0] == 0:
+                objWidz = falseImg[i][0]
+            if tmpWidz.shape[0] == 0:
+                tmpWidz = falseImg[i][1]
+            if resiWidz.shape[0] == 0:
+                resiWidz = falseImg[i][2]
+            plt.clf()
+            fig, axes = plt.subplots(1, 3, figsize=(6, 2))
+            axes.flat[0].imshow(objWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[1].imshow(tmpWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[2].imshow(resiWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[2].set_title("predicted pbb = " + str(np.round(truePred[i][1], 2)) + 
+                                          ", label = " + str(trueLabel[i, 1]) + 
+                                          ", s2n = " + str(trueS2n[i]))
+            plt.show()
+            tnum = tnum + 1
+            if tnum>showNum:
+                break
+    
+    tnum = 0
+    print("\n\n***********************")
+    print("image of False classified as False")
+    for i in range(trueImg.shape[0]):
+        if trueLabel[i, 1]==0:
+            objWidz = zscale_image(trueImg[i][0])
+            tmpWidz = zscale_image(trueImg[i][1])
+            resiWidz = zscale_image(trueImg[i][2])
+            if objWidz.shape[0] == 0:
+                objWidz = falseImg[i][0]
+            if tmpWidz.shape[0] == 0:
+                tmpWidz = falseImg[i][1]
+            if resiWidz.shape[0] == 0:
+                resiWidz = falseImg[i][2]
+            plt.clf()
+            fig, axes = plt.subplots(1, 3, figsize=(6, 2))
+            axes.flat[0].imshow(objWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[1].imshow(tmpWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[2].imshow(resiWidz, interpolation = "nearest", cmap='gray')
+            axes.flat[2].set_title("predicted pbb = " + str(np.round(truePred[i][1], 2)) + 
+                                          ", label = " + str(trueLabel[i, 1]) + 
+                                          ", s2n = " + str(trueS2n[i]))
+            plt.show()
+            tnum = tnum + 1
+            if tnum>showNum:
+                break
     
 if __name__ == "__main__":
     
