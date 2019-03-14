7852e4f00fea256b60bd39bfc738ceb8967743d9
archord <xyag.902@163.com>
2019-03-15 00:42:45 +0800

    request post files

---
+++ gwac_pipeline/getCatlogList.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
+import os
+import psycopg2
+
+
+class CatalogList:
+    
+    connParam={
+        "host": "10.0.82.111",
+        "port": "5432",
+        "dbname": "gwac",
+        "user": "postgres",
+        "password": "gwac1234"
+        }
+    
+    def __init__(self):
+        
+        self.conn = psycopg2.connect(**self.connParam)
+        self.dataVersion = ()
+        
+    def closeDb(self):
+        self.conn.close()
+        
+    def getRecord(self):
+        
+        sql = "select objCat.oc_id, objCat.name, objCat.path, objCat.date_ut, objCat.cam_name, objCat.sky_name, tmpCat.name tmpName, tmpCat.path tmpPath "\
+            "from object_catalog objCat "\
+            "INNER JOIN template_catalog tmpCat on objCat.tc_id=tmpCat.tc_id "\
+            "where objCat.is_upload=true and objCat.is_process=false "\
+            "limit 20"
+
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        for trow in rows:
+            print(trow)
+            objCatId = trow[0]
+            tsql = "update object_catalog set is_process=true where oc_id=%d"%(objCatId)
+            cur.execute(tsql)
+            cur.commit()
+            
+        cur.close()
+        
+
+            
+if __name__ == '__main__':
+    
+    versionName = "version1"
+    mr = CatalogList()
+    mr.getRecord()
+    mr.closeDb()
+    
+    
