256da83658a2ba12198e10d14478e4032d90524c
archord <xyag.902@163.com>
2019-03-22 16:08:23 +0800

    backup

--- gwac_pipeline/getCatlogList.py
+++ gwac_pipeline/getCatlogList.py
@@ -23,6 +23,12 @@ class CatalogList:
         
     def closeDb(self):
         self.conn.close()
+    
+    def reProcessAll(self):
+        sql="update object_catalog set is_process=false"
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        cur.close()
         
     def getRecord(self):
         
@@ -30,6 +36,7 @@ class CatalogList:
             "from object_catalog objCat "\
             "INNER JOIN template_catalog tmpCat on objCat.tc_id=tmpCat.tc_id "\
             "where objCat.is_upload=true and objCat.is_process=false "\
+            "ORDER BY objCat.date_ut asc "\
             "limit 20"
 
         cur = self.conn.cursor()
@@ -39,8 +46,9 @@ class CatalogList:
             print(trow)
             objCatId = trow[0]
             tsql = "update object_catalog set is_process=true where oc_id=%d"%(objCatId)
+            print(tsql)
             cur.execute(tsql)
-            cur.commit()
+            self.conn.commit()
             
         cur.close()
         
