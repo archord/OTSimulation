b6896c505daa80bd29fd7c6a325d56595cb7d7a2
archord <xyag.902@163.com>
2019-06-26 00:22:21 +0800

    backup

--- matchTest/plotAffine.py
+++ matchTest/plotAffine.py
@@ -28,14 +28,14 @@ def getData(srcDir, fname, destDir):
     tiNum, oiNum, oiJNum,tiJNum, mchNum = tdata[:,7],tdata[:,8],tdata[:,9],tdata[:,10],tdata[:,11]
     mratio0, mratio1, mratio2 = tdata[:,12],tdata[:,21],tdata[:,30]
     ccdNums = tdata[:,37].astype(np.int)
-    
+    ''' 
     tIdx = ccdNums==ccdNum
     oiNum = oiNum[tIdx] 
     mratio0 = mratio0[tIdx] 
     mratio1 = mratio1[tIdx] 
     mratio2 = mratio2[tIdx] 
     print(mratio0.shape)
-    
+    '''
     return oiNum, mratio0, mratio1, mratio2
                 
         
@@ -43,10 +43,10 @@ def run1():
     
     srcPath = 'data'
     destPath = "draw"
-    fname1 = 'G021_mon_objt_181101T17255569_mch_statistic.cat'
-    fname2 = 'G032_mon_objt_190110T14080401_mch_statistic.cat'
-    fname3 = 'G043_mon_objt_190126T10594812_mch_statistic.cat'
-    fname4 = 'G024_mon_objt_181018T18570151_mch_statistic.cat'
+    fname1 = 'G021_mon_objt_181101T17255569_mch_statistic4000.cat'
+    fname2 = 'G032_mon_objt_190110T14080401_mch_statistic4000.cat'
+    fname3 = 'G043_mon_objt_190126T10594812_mch_statistic4000.cat'
+    fname4 = 'G024_mon_objt_181018T18570151_mch_statistic4000.cat'
     
     if not os.path.exists(destPath):
         os.system("mkdir -p %s"%(destPath))            
@@ -56,6 +56,23 @@ def run1():
     oiNum2, mratio02, mratio12, mratio22 = getData(srcPath, fname2, destPath)
     oiNum3, mratio03, mratio13, mratio23 = getData(srcPath, fname3, destPath)
     oiNum4, mratio04, mratio14, mratio24 = getData(srcPath, fname4, destPath)
+    
+    fig, axes = plt.subplots(2,2,figsize=(8,6))
+    axs= axes.ravel()
+    
+    axs[0].plot(oiNum1, mratio21, '.', label='CCD-1')
+    axs[0].grid()
+    axs[0].legend(loc='lower right', framealpha=0.5)
+    axs[1].plot(oiNum2, mratio22, '.', label='CCD-2')
+    axs[1].grid()
+    axs[1].legend(loc='lower right', framealpha=0.5)
+    axs[2].plot(oiNum3, mratio23, '.', label='CCD-3')
+    axs[2].grid()
+    axs[2].legend(loc='lower right', framealpha=0.5)
+    axs[3].plot(oiNum4, mratio24, '.', label='CCD-4')
+    axs[3].grid()
+    axs[3].legend(loc='lower right', framealpha=0.5)
+    plt.show()
     
     fig, axes = plt.subplots(2,2,figsize=(8,6))
     axs= axes.ravel()
--- matchTest/plotAnalysis.py
+++ matchTest/plotAnalysis.py
@@ -10,13 +10,13 @@ import traceback
 from matplotlib.ticker import FuncFormatter
 import pandas as pd
 
-def unionStatistic():
+def unionStatistic2():
     
     srcDir = 'data'
-    fname1 = 'G021_mon_objt_181101T17255569_mch_statistic2.cat'
-    fname2 = 'G032_mon_objt_190110T14080401_mch_statistic2.cat'
-    fname3 = 'G043_mon_objt_190126T10594812_mch_statistic2.cat'
-    fname4 = 'G024_mon_objt_181018T18570151_mch_statistic2.cat'
+    fname1 = 'G021_mon_objt_181101T17255569_mch_statistic4000.cat'
+    fname2 = 'G032_mon_objt_190110T14080401_mch_statistic4000.cat'
+    fname3 = 'G043_mon_objt_190126T10594812_mch_statistic4000.cat'
+    fname4 = 'G024_mon_objt_181018T18570151_mch_statistic4000.cat'
     
     tdata1 = np.loadtxt("%s/%s"%(srcDir, fname1), dtype='str')
     print(tdata1.shape)
@@ -38,10 +38,12 @@ def unionStatistic():
     
     blindStarNum, featurePointNum = tdata[:,5],tdata[:,6]
     mratio0, mratio1, mratio2 = tdata[:,12],tdata[:,21],tdata[:,30]
+    fwhmMean, bkgMean = tdata[:,38],tdata[:,40]
+    tiNum, oiNum, oiJNum,tiJNum, mchNum = tdata[:,7],tdata[:,8],tdata[:,9],tdata[:,10],tdata[:,11]
     tIdx = (blindStarNum>0) & (featurePointNum>0) & (mratio2>80)
     tdata80 = tdata[tIdx]
     print(tdata80.shape)
-    tIdx = (blindStarNum>0) & (featurePointNum>0) & (mratio2<=60)
+    tIdx = (blindStarNum>0) & (featurePointNum>0) & (mratio2>=60)
     tdata = tdata[tIdx]
     print(tdata.shape)
     
@@ -55,7 +57,7 @@ def unionStatistic():
     xshift1,yshift1, xrms1, yrms1 = tdata[:,22],tdata[:,23],tdata[:,24],tdata[:,25]
     xshift2,yshift2, xrms2, yrms2 = tdata[:,31],tdata[:,32],tdata[:,33],tdata[:,34]
     blindMchTime = tdata[:,-1]
-    
+    '''
     plt.hist(mchNum,20)
     plt.title("mchNum")
     plt.grid()
@@ -105,7 +107,7 @@ def unionStatistic():
     plt.title("yrms2")
     plt.grid()
     plt.show()
-    
+    '''
 def magStatistic1(srcDir, fname, destDir):
     
     try:
@@ -142,6 +144,75 @@ def magStatistic1(srcDir, fname, destDir):
         tstr = traceback.format_exc()
         print(tstr)
         
+def unionStatistic(tnum=4000):
+    
+    srcDir = 'data'
+    fname1 = 'G021_mon_objt_181101T17255569_mch_statistic%d.cat'%(tnum)
+    fname2 = 'G032_mon_objt_190110T14080401_mch_statistic%d.cat'%(tnum)
+    fname3 = 'G043_mon_objt_190126T10594812_mch_statistic%d.cat'%(tnum)
+    fname4 = 'G024_mon_objt_181018T18570151_mch_statistic%d.cat'%(tnum)
+    
+    tdata1 = np.loadtxt("%s/%s"%(srcDir, fname1), dtype='str')
+    tdata2 = np.loadtxt("%s/%s"%(srcDir, fname2), dtype='str')
+    tdata3 = np.loadtxt("%s/%s"%(srcDir, fname3), dtype='str')
+    tdata4 = np.loadtxt("%s/%s"%(srcDir, fname4), dtype='str')
+    
+    tdata = np.concatenate([tdata1,tdata2,tdata3,tdata4])
+    totalFileNum = tdata.shape[0]
+    
+    fname = tdata[:,0].copy()
+    dateStr = tdata[:,36].copy()
+    tdata[:,0] = '0'
+    tdata[:,36] = '0'
+    tdata = tdata.astype(np.float)
+    
+    blindStarNum, featurePointNum = tdata[:,5],tdata[:,6]
+    mratio0, mratio1, mratio2 = tdata[:,12],tdata[:,21],tdata[:,30]
+    fwhmMean, bkgMean = tdata[:,38],tdata[:,40]
+    tiNum, oiNum, oiJNum,tiJNum, mchNum = tdata[:,7],tdata[:,8],tdata[:,9],tdata[:,10],tdata[:,11]
+    xshift,yshift, xrotation, yrotation = tdata[:,1],tdata[:,2],tdata[:,3],tdata[:,4]
+    
+    
+    print(totalFileNum)
+    tIdx01 = (oiNum > 4000)
+    tIdx02 = (oiNum > 0.8*tiNum)
+    oiImgNum1 = np.sum(tIdx01)
+    oiImgNum2 = np.sum(tIdx02)
+    print("%d,%.2f;%d,%.2f"%(oiImgNum1,oiImgNum1*100.0/totalFileNum,oiImgNum2,oiImgNum2*100.0/totalFileNum))
+
+    fwhmT = 3
+    tIdx11 = tIdx01 & (fwhmMean<fwhmT) #fwhm=2.5,3,5;0.910174,0.950021,0.990278
+    tIdx12 = tIdx02 & (fwhmMean<fwhmT) #fwhm=2.5,3,5;0.910174,0.950021,0.990278
+    goodImgNum1 = np.sum(tIdx11)
+    goodImgNum2 = np.sum(tIdx12)
+    print("%d,%.2f;%d,%.2f"%(goodImgNum1,goodImgNum1*100.0/totalFileNum,goodImgNum2,goodImgNum2*100.0/totalFileNum))
+
+    mratioT = 70
+    tIdx21 = tIdx11 & (mratio2>mratioT)
+    tIdx22 = tIdx12 & (mratio2>mratioT)
+    goodMatch1 = np.sum(tIdx21)
+    goodMatch2 = np.sum(tIdx22)
+    print("%d,%.2f;%d,%.2f"%(goodMatch1,goodMatch1*100.0/goodImgNum1,goodMatch2,goodMatch2*100.0/goodImgNum2))
+    
+    mratioT = 70
+    tIdx31 = tIdx11 & (mratio2<mratioT)
+    tIdx32 = tIdx12 & (mratio2<mratioT)
+
+    xshift = xshift[tIdx21]
+    yshift = yshift[tIdx21]
+    mratio2 = mratio2[tIdx21]
+    
+    ''' '''
+    plt.plot(xshift, mratio2,'.')
+    plt.title("mchNum1")
+    plt.grid()
+    plt.show()
+    
+    plt.plot(yshift, mratio2,'.')
+    plt.title("mchNum2")
+    plt.grid()
+    plt.show()
+    
 def run1():
     
     srcPath = 'data'
@@ -165,4 +236,5 @@ def run1():
 if __name__ == "__main__":
     
     #run1()
-    unionStatistic()
+    unionStatistic(4000)
+    #unionStatistic(8000)
